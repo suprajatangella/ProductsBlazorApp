99b9702c131c63d08c5bfdd49048f420cddfa82e
Supraja Tangella <supraja.tangella@gmail.com>
2024-05-15 20:34:02 +0100

    My First Blazor application

---
+++ ProductsBlazorApp/ProdItem.cs
@@ -0,0 +1,11 @@
+﻿
+namespace ProductsBlazorApp;
+
+public class ProdItem
+{
+    public int ProductId { get; set; }
+    public string Name { get; set; }
+    public double Price { get; set; }
+    public string Description { get; set; }
+    public string CategoryName { get; set; }
+}
